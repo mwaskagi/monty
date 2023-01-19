0ac62c41bdef511d851ed2b984cfc2903965c61b
mwaskagi <gituiku.mwangi@gmail.com>
2023-01-19 08:58:24 -0800

    Monty Push and Pall

---
+++ README.md
@@ -0,0 +1,1 @@
+Monty Data structures
---
+++ monty.h
@@ -0,0 +1,57 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern char *number;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack
+ * (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: point to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure for stack
+ * queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void readfile(char *myfile);
+char **validateBuffer(char *buffer, int pointer, FILE *montyF);
+void op_val(char **command, char *buffer, stack_t **list, int line, FILE *a);
+void f_opcode(char **command, char *buffer, int line, stack_t **list, FILE *m);
+int _memory(char *buffer);
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_sub(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t **stack);
+
+#endif
